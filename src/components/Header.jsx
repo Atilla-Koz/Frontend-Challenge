30d61a14d5e0a439d9dac27335e7b0f89b67476a
Atilla-Koz <atillakoz@hotmail.com>
2024-04-06 18:59:57 +0300

    Refactor CSS styles and update header and hero components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
+import Text from '../assets/header/A.png';
 import Logo from '../assets/header/Ellipse 9.png';
 import '../App.css';
 export default function Header() {
   return (
-    <div className="headerContainer">
-      <img src={Logo} alt="Logo" />
-      <p>Skills</p>
-      <p>Projeckts</p>
-      <p>Hire me</p>
-    </div>
+    <header className="headerHeader">
+      <section className="logoContainer">
+        <img className="hLogo" src={Logo} alt="Logo" />
+        <img className="hText" src={Text} alt="Text" />
+      </section>
+      <section className="hNav">
+        <p>Skills</p>
+        <p>Projeckts</p>
+        <p>Hire me</p>
+      </section>
+    </header>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,25 @@
 export default function Hero() {
   return (
-    <section>
-      <h2>Hero</h2>
-    </section>
+    <div className="heroContainer">
+      <section className="heroContainerP">
+        <p className="heroP">Atilla Köz</p>
+      </section>
+      <section className="heroBot">
+        <div className="text">
+          <h1>Frontend Developer</h1>
+          <p>
+            Hi, I'm Almila. I'm a full-stack developer. If you are looking for a
+          </p>
+          <p>
+            Developer who to craft solid and scalable frontend products with
+          </p>
+          <p>great user experiences. Let's shake hands with me.</p>
+          <button>Hire Me</button>
+          <button>Git Hub</button>
+          <button>Linkdin</button>
+        </div>
+        <div className="heroImg"></div>
+      </section>
+    </div>
   );
 }

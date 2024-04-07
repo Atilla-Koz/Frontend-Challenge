7259a75bf505ae85e712b0b5796f37968c82a46c
Atilla-Koz <atillakoz@hotmail.com>
2024-04-07 20:26:34 +0300

    Refactor CSS styles, update header and hero components, and create new hero image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,14 @@
+import img from '../assets/hero/image 1.png';
+
 export default function Hero() {
   return (
     <div className="heroContainer">
-      <section className="heroContainerP">
-        <p className="heroP">Atilla Köz</p>
+      <section className="heroContainerTitle">
+        <h2 className="heroName">Atilla Köz</h2>
+        <h1>Frontend Developer</h1>
       </section>
       <section className="heroBot">
-        <div className="text">
-          <h1>Frontend Developer</h1>
+        <div className="heroText">
           <p>
             Hi, I'm Almila. I'm a full-stack developer. If you are looking for a
           </p>
@@ -14,11 +16,15 @@ export default function Hero() {
             Developer who to craft solid and scalable frontend products with
           </p>
           <p>great user experiences. Let's shake hands with me.</p>
-          <button>Hire Me</button>
-          <button>Git Hub</button>
-          <button>Linkdin</button>
+          <section className="heroButtons">
+            <button>Hire Me</button>
+            <button>Git Hub</button>
+            <button>Linkdin</button>
+          </section>
         </div>
-        <div className="heroImg"></div>
+        <div className="heroImg">
+          # <img src={img} alt="hero" />
+        </div>
       </section>
     </div>
   );

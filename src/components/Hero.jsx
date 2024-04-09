218924c8567c9e7fe0c6ddb968bedb981ee80b54
Atilla-Koz <atillakoz@hotmail.com>
2024-04-09 17:07:02 +0300

    Refactor CSS styles, update header and hero components, and add new hero image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,37 @@
 import img from '../assets/hero/image 1.png';
+import git from '../assets/hero/github.png';
+import linkedin from '../assets/hero/LinkedIn.png';
 
 export default function Hero() {
   return (
     <div className="heroContainer">
-      <section className="heroContainerTitle">
-        <h2 className="heroName">Atilla Köz</h2>
-        <h1>Frontend Developer</h1>
-      </section>
       <section className="heroBot">
         <div className="heroText">
-          <p>
+          <section className="heroContainerTitle">
+            <h2 className="heroName">Atilla Köz</h2>
+            <h1 className="heroTitle">Frontend Developer</h1>
+          </section>
+          <p className="heroP">
             Hi, I'm Almila. I'm a full-stack developer. If you are looking for a
-          </p>
-          <p>
             Developer who to craft solid and scalable frontend products with
+            great user experiences. Let's shake hands with me.
           </p>
-          <p>great user experiences. Let's shake hands with me.</p>
-          <section className="heroButtons">
-            <button>Hire Me</button>
-            <button>Git Hub</button>
-            <button>Linkdin</button>
+          <section className="heroButtonsContainer">
+            <button className="heroButtonsHere">Hire Me</button>
+            <a href="https://github.com/Atilla-Koz">
+              <button className="heroButtons">
+                <img className="gitIcon" src={git}></img>GitHub
+              </button>
+            </a>
+            <a href="https://www.linkedin.com/in/atilla-k%C3%B6z-9b1841194/">
+              <button className="heroButtons">
+                <img className="gitIcon" src={linkedin}></img>Linkdin
+              </button>
+            </a>
           </section>
         </div>
-        <div className="heroImg">
-          # <img src={img} alt="hero" />
+        <div className="heroImgContainer">
+          <img className="heroImg" src={img} alt="hero" />
         </div>
       </section>
     </div>

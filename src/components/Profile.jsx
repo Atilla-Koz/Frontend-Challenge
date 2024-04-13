d7e7da2b6a80ed60022d340ca653fc226712aa1e
Atilla-Koz <atillakoz@hotmail.com>
2024-04-13 16:45:45 +0300

    Add profile CSS styles and update Profile component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,26 @@
 export default function Profile() {
   return (
-    <section>
-      <h2>Profile</h2>
-    </section>
+    <div>
+      <h2 className="profileTitle">Profile</h2>
+      <section className="ProfileContainer">
+        <div className="personInfo">
+          <h3 className="InfoTitle">Profile</h3>
+          <p className="InfoText">
+            Doğum tarihi <p className="InfoTextData">20.10.1998</p>
+          </p>
+          <p className="InfoText">
+            İkamet Şehir
+            <p className="InfoTextData">Istanbul</p>
+          </p>
+          <p className="InfoText">
+            Eğitim Durumu
+            <p className="InfoTextData"> Istanbul Kultur University</p>
+          </p>
+          <p className="InfoText">
+            Tercih Ettiği Rol <p className="InfoTextData">Frontend</p>
+          </p>
+        </div>
+      </section>
+    </div>
   );
 }

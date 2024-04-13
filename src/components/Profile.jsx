fef1dfb33c05103ee020424becb715e34eaffea1
Atilla-Koz <atillakoz@hotmail.com>
2024-04-13 19:33:36 +0300

    Update profile CSS styles and component, and add about me section

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -6,21 +6,38 @@ export default function Profile() {
         <div className="personInfo">
           <h3 className="InfoTitle">Profile</h3>
           <p className="InfoText">
-            Doğum tarihi <p className="InfoTextData">20.10.1998</p>
+            Doğum tarihi{' '}
+            <span className="InfoTextData">&nbsp;&nbsp;&nbsp;20.10.1998</span>
           </p>
           <p className="InfoText">
-            İkamet Şehir
-            <p className="InfoTextData">Istanbul</p>
+            İkamet Şehir{' '}
+            <span className="InfoTextData">
+              &nbsp;&nbsp;&nbsp;&nbsp;Istanbul
+            </span>
           </p>
           <p className="InfoText">
             Eğitim Durumu
-            <p className="InfoTextData"> Istanbul Kultur University</p>
+            <span className="InfoTextData">Istanbul Kultur University</span>
           </p>
           <p className="InfoText">
-            Tercih Ettiği Rol <p className="InfoTextData">Frontend</p>
+            Tercih Ettiği Rol <span className="InfoTextData">Frontend</span>
           </p>
         </div>
+        <div className="aboutMe">
+          <h2 className="aboutTitle">About Me</h2>
+          <span className="aboutMeTexts">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam
+              aut, odit laborum aliquam voluptatum nisi mollitia.
+            </p>
+            <p>
+              Mnima accusamus ratione soluta aperiam sit voluptate? Dicta quod
+              deserunt quam temporibus cumque magnam!
+            </p>
+          </span>
+        </div>
       </section>
+      <hr className="skillsHr" />
     </div>
   );
 }

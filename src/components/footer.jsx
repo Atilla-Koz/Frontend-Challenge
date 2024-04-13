e0721f30641cb015b2675c75f4e349ae62fe4e35
Atilla-Koz <atillakoz@hotmail.com>
2024-04-13 21:00:27 +0300

    Add footer CSS styles and update Footer component

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,26 @@
 export default function Footer() {
   return (
-    <footer>
-      <h2>Footer</h2>
+    <footer className="footerContainer">
+      <h2 className="footerTitle">Let’s work together on your next product.</h2>
+      <div className="footerLinks">
+        <a className="email" href="atillakoz@hotmail.com">
+          atillakoz@hotmail.com
+        </a>
+        <div className="footerSocial">
+          <a className="personalblog" href="atillakoz@hotmail.com">
+            Personal Blog
+          </a>
+          <a className="projeckGithub" href="https://github.com/Atilla-Koz">
+            GitHub
+          </a>
+          <a
+            className="projecktLinkedin"
+            href="https://www.linkedin.com/in/atilla-k%C3%B6z-9b1841194/"
+          >
+            Linkedin
+          </a>
+        </div>
+      </div>
     </footer>
   );
 }

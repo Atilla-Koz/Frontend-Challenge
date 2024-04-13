3cfe9ec31cbcf6514fb66d93dee16e57a537966a
Atilla-Koz <atillakoz@hotmail.com>
2024-04-13 20:20:15 +0300

    Update profile CSS styles and component, and add Projeckts section

--- src/components/Projeckts.jsx
+++ src/components/Projeckts.jsx
@@ -1,7 +1,91 @@
+import projecktImg from '../assets/Projeckts/projeckt.png';
 export default function Projeckts() {
   return (
     <section>
-      <h2>Projeckts</h2>
+      <h2 className="ProjecktsTitle">Projeckts</h2>
+      <div className="ProjecktsContainar">
+        <div className="Projeckt">
+          <img className="projecktImg" src={projecktImg} alt="Projeckt" />
+          <h3 className="projecktTitle">Random Jokes</h3>
+          <p className="projecktText">
+            A simple, customizable, minimal setup cookie plugin that allows your
+            users to select which cookies to accept or decline. This was created
+            with vanilla JS, SCSS and Parcel Bundler and is available as a NPM
+            package and the git repository makes any type of customization to
+            code and themes possible
+          </p>
+          <div className="projeckIns">
+            <p className="projecktIN">React</p>
+            <p className="projecktIN">Redux</p>
+            <p className="projecktIN">Axios</p>
+          </div>
+          <div className="ProjecktLinks">
+            <a className="projecktLink" href="https://github.com/Atilla-Koz">
+              GitHub
+            </a>
+            <a
+              className="projecktLink"
+              href="https://www.linkedin.com/in/atilla-k%C3%B6z-9b1841194/"
+            >
+              linkedin
+            </a>
+          </div>
+        </div>
+        <div className="Projeckt">
+          <img className="projecktImg" src={projecktImg} alt="Projeckt" />
+          <h3 className="projecktTitle">Random Jokes</h3>
+          <p className="projecktText">
+            A simple, customizable, minimal setup cookie plugin that allows your
+            users to select which cookies to accept or decline. This was created
+            with vanilla JS, SCSS and Parcel Bundler and is available as a NPM
+            package and the git repository makes any type of customization to
+            code and themes possible
+          </p>
+          <div className="projeckIns">
+            <p className="projecktIN">React</p>
+            <p className="projecktIN">Redux</p>
+            <p className="projecktIN">Axios</p>
+          </div>
+          <div className="ProjecktLinks">
+            <a className="projecktLink" href="https://github.com/Atilla-Koz">
+              GitHub
+            </a>
+            <a
+              className="projecktLink"
+              href="https://www.linkedin.com/in/atilla-k%C3%B6z-9b1841194/"
+            >
+              linkedin
+            </a>
+          </div>
+        </div>
+        <div className="Projeckt">
+          <img className="projecktImg" src={projecktImg} alt="Projeckt" />
+          <h3 className="projecktTitle">Random Jokes</h3>
+          <p className="projecktText">
+            A simple, customizable, minimal setup cookie plugin that allows your
+            users to select which cookies to accept or decline. This was created
+            with vanilla JS, SCSS and Parcel Bundler and is available as a NPM
+            package and the git repository makes any type of customization to
+            code and themes possible
+          </p>
+          <div className="projeckIns">
+            <p className="projecktIN">React</p>
+            <p className="projecktIN">Redux</p>
+            <p className="projecktIN">Axios</p>
+          </div>
+          <div className="ProjecktLinks">
+            <a className="projecktLink" href="https://github.com/Atilla-Koz">
+              GitHub
+            </a>
+            <a
+              className="projecktLink"
+              href="https://www.linkedin.com/in/atilla-k%C3%B6z-9b1841194/"
+            >
+              linkedin
+            </a>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }

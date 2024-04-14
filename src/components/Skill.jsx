848dea3d813dd62333f6d2e5545cbbe56fe2d183
Atilla-Koz <atillakoz@hotmail.com>
2024-04-14 16:57:26 +0300

    Refactor CSS styles, update Skill component, and tailwind update

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,31 +1,39 @@
 export default function Skill() {
   return (
     <section className="skillsContainer">
-      <h2 className="SkillTitle">Skills</h2>
-      <div className="skillsCards">
+      <h2 className="text-title font-semibold text-5xl leading-12 w-1039 h-64 ml-left-5">
+        Skills
+      </h2>
+      <div className="flex md:flex-row justify-around gap-4 pl-5p pr-5p flex-col">
         <div className="skillsCard">
-          <h3 className="cardTitle">Java Script</h3>
-          <p className="cardText">
+          <h3 className="font-medium h-7 text-customPurple1 text-3xl leading-7 py-12">
+            Java Script
+          </h3>
+          <p className="text-customGray font-normal text-base leading-normal">
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
             officia deserunt mollit anim id est laborum.
           </p>
         </div>
         <div className="skillsCard">
-          <h3 className="cardTitle">Java Script</h3>
-          <p className="cardText">
+          <h3 className="font-medium h-7 text-customPurple1 text-3xl leading-7 py-12">
+            Java Script
+          </h3>
+          <p className="text-customGray font-normal text-base leading-normal">
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
             officia deserunt mollit anim id est laborum.
           </p>
         </div>
         <div className="skillsCard">
-          <h3 className="cardTitle">Java Script</h3>
-          <p className="cardText">
+          <h3 className="font-medium h-7 text-customPurple1 text-3xl leading-7 py-12">
+            Java Script
+          </h3>
+          <p className="text-customGray font-normal text-base leading-normal">
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
             officia deserunt mollit anim id est laborum.
           </p>
         </div>
       </div>
-      <hr className="skillsHr" />
+      <hr className="m-20 border border-line" />
     </section>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,27 @@ module.exports = {
     extend: {
       colors: {
         customPurple: '#3730a3',
+        customPurple1: '#4338ca',
         customGray: '#6b7280',
         title: '#1f2937',
+        line: '#bab2e7',
       },
       margin: {
         'left-25': '25%',
         'right-25': '25%',
         'left-33': '33%',
+        'left-5': '5%',
+      },
+
+      spacing: {
+        '5p': '5%',
+      },
+      width: {
+        1039: '1039px',
+        236: '236px',
+      },
+      height: {
+        64: '64px',
       },
     },
   },

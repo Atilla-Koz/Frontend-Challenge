bc54efca434cbe048712f6fcbbce9996c48411c9
Atilla-Koz <atillakoz@hotmail.com>
2024-04-15 20:07:14 +0300

    Refactor CSS styles, update Mode component, and add modal CSS styles

--- src/components/Mode.jsx
+++ src/components/Mode.jsx
@@ -1,8 +1,8 @@
 export default function Mode() {
   return (
-    <div className="modeContainer">
+    <div className="flex flex-row justify-around pt-4 pb-4">
       <div></div>
-      <div className="soul">
+      <div className="flex flex-row gap-4">
         <label className="inline-flex items-center cursor-pointer">
           <input
             type="checkbox"
@@ -10,7 +10,7 @@ export default function Mode() {
             className="sr-only peer"
             defaultChecked
           />
-          <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-customPurple"></div>
           <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
             Dark Mode
           </span>

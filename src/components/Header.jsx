04a6f02c51b9d12326b8ce92c31f5c825e314582
Atilla-Koz <atillakoz@hotmail.com>
2024-04-19 13:04:20 +0300

    Refactor Header component to add smooth scrolling functionality and update Mode component to include language in API request

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ export default function Header() {
         <img className="hLogo" src={Logo} alt="Logo" />
         <img className="absolute top-1/3 left-5" src={Text} alt="Text" />
       </section>
-      <section className="flex flex-row gap-16 items-center">
+      <section className="flex flex-row sm:gap-16 gap-6 items-center">
         <button
           className="dark:text-customGray"
           onClick={() => handleScroll(400)}
@@ -31,7 +31,7 @@ export default function Header() {
         >
           {data[language].headerData.projects}
         </button>
-        <button className="flex flex-row text-customPurple border-2 border-customPurple rounded-lg text-lg font-medium leading-7 px-5 py-2 dark:bg-white">
+        <button className="flex flex-row text-customPurple border-2 border-customPurple rounded-lg text-lg font-medium leading-7 sm:px-5 px-1 py-2 dark:bg-white">
           {data[language].headerData.hireMe}
         </button>
       </section>
